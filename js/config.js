1eeecc2221f124da21c18e9fea19b7bff107cbcd
I JeAnK I <jeankchamputis12@gmail.com>
2026-09-22 19:20:10 -0500

    feat: configuracion de Supabase y soporte hibrido para GitHub Pages

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// js/config.js
+const CONFIG = {
+    SUPABASE_URL: "https://cyytkxivddujuawxeaiu.supabase.co", // Valor de SUPABASE_URL
+    SUPABASE_KEY: "sb_publishable_5I6VTXDY3SucdXlAVwhqSQ_N2L1bnNr",             // Valor de SUPABASE_PUBLISHABLE_KEY
+    
+    // Detecta automáticamente si está corriendo en GitHub Pages
+    ES_NUBE: window.location.hostname.includes("github.io")
+};
